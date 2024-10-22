80eac4d5d7a15e8bfd5a9a9164fcb4c8a7f4b3a7
wangjiahao <1332661444@qq.com>
2024-10-22 18:55:40 +0800

    feat: add sync-git-multi-branches.py

---
+++ sync-git-multi-branches.py
@@ -0,0 +1,97 @@
+import os
+import subprocess
+
+# 仅支持linux系统,并且保证安装过git,否则会报错, 如: yum install -y git
+
+
+# 文件路径
+file_path = "/home/sync-git/"
+
+# 仓库信息
+repo_list = [
+    {
+        # 仓库名
+        "repo_name": "sync-git",
+        # 需要同步的分支列表
+        "branches": ["main", "dev"],
+        # 公有仓库(源仓库)
+        "public_repo": "https://gitlab.top/test/sync-git.git",
+        # 私有仓库(从仓库)
+        "private_repo": "https://gitlab.com/test/sync-git.git"
+    }
+]
+
+# 证书信息, 格式 https://账户名:密码@域名
+credential_list = [
+    'https://admin:12345678@gitlab.com', # 仓库1证书信息
+    'https://admin:12345678@gitlab.org' # 仓库2证书信息
+]
+
+
+def set_git_credentials():
+    """
+    设置 Git 凭证存储
+    """
+    credentials_file = os.path.expanduser("~/.git-credentials")
+     # 检查文件是否存在，如果不存在则创建
+    if not os.path.exists(credentials_file):
+        # 设置 Git 凭证为store
+        subprocess.run(["git", "config", "--global", "credential.helper", "store"])
+        try:
+            with open(credentials_file, 'w') as file:
+                for credential in credential_list:
+                    file.write(f"{credential}\n")
+            print(f"凭证已更新到 {credentials_file}")
+        except IOError:
+            print("IO Error occurred when writing to the file.")
+
+def git_clone(repo):
+    """
+    克隆git仓库
+    """
+    for branch in repo["branches"]:
+        result = subprocess.run(["git", "clone", "-b", branch, repo["public_repo"]])
+        if result.returncode != 0:
+            print(f"克隆 {branch} 分支时发生错误.")
+            return False
+        print(f"{branch} 分支克隆成功:", repo["repo_name"])
+    return True
+
+def git_sync(repo):
+    """
+    同步git仓库
+    """
+    for branch in repo["branches"]:
+        # 切换到分支
+        subprocess.run(["git", "checkout", branch])
+
+        # 拉取最新的源仓库内容
+        result = subprocess.run(["git", "pull", "origin", branch])
+        if result.returncode != 0:
+            print(f"从源仓库拉取 {branch} 分支时发生错误.")
+            return
+        # 将源仓库的内容推送到目标仓库
+        result = subprocess.run(["git", "push", repo["private_repo"], branch])
+        if result.returncode != 0:
+            print(f"将内容推送到 {repo['private_repo']} 的 {branch} 分支时发生错误.")
+            return
+
+def sync():
+    """
+    同步任务
+    """
+    for repo in repo_list:
+        os.chdir(file_path)
+        # 检查仓库是否已经存在
+        full_repo_path = os.path.join(file_path, repo["repo_name"])
+        if not os.path.exists(full_repo_path):
+            cloned = git_clone(repo)
+            if not cloned:
+                continue
+        os.chdir(full_repo_path)
+        git_sync(repo)
+
+# 调用函数
+if __name__ == "__main__":
+    set_git_credentials()
+    sync()
